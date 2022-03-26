330f1ab1a278b638093a4ea5bb52028c32216a31
fumiyamiyazaki <57476802+fumiyamiyazaki@users.noreply.github.com>
2022-03-26 17:24:08 +0900

    get users dispay list of users

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,35 @@
-import { memo, VFC } from "react";
+/** eslint-disable react-hooks/exhaustive-deps */
+
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
+
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページ</p>;
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), [getUsers]);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((users) => (
+            <WrapItem key={users.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={users.username}
+                fullName={users.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,9 +3,11 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const navigate = useNavigate();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
 
@@ -17,15 +19,18 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             navigate("/home");
           } else {
-            alert("not found user");
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
           }
         })
-        .catch(() => alert("can not login"))
+        .catch(() =>
+          showMessage({ title: "ログインできません", status: "error" })
+        )
         .finally(() => setLoading(false));
     },
-    [navigate]
+    [navigate, showMessage]
   );
 
   return { login, loading };
